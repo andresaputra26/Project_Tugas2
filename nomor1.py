dcfd816ba6af091a9e958b1583d937e7f203c034
Andre Saputra Ginting <115556206+andresaputra26@users.noreply.github.com>
2022-10-27 16:32:41 +0700

    Delete nomor1.py

--- nomor1.py
+++
@@ -1,5 +0,0 @@
-print("1. Jelaskan Type data dalam bahasa dasar pemrograman dan berikan contoh data");
-print("a. Integer, \n bilangan bulat, \n Contoh : 1, 5, 10");
-print("b. boolean, \n Tipe data boolean hanya memiliki dua buah nilai, \n yaitu; True dan False.");
-print("c. float, \n bilangan pecahan, \n contoh 1.3, 4.2, 22.3,");
-print("d. string, \n Kumpulan karakter, \n contoh 'Aku Lagi kuliah'");
