03ef28be52c53bca44fe7b3b106db4ee78548c1f
Andre Saputra Ginting <andresaputra260604@gmail.com>
2022-10-27 16:15:42 +0700

    Tugas UTS Dasar Pemrograman
    Tugas UTS

---
+++ nomor4_Penggunaan for.py
@@ -0,0 +1,13 @@
+#penggunaan for angka
+print("penggunaan for angka : ")
+for i in range(100):
+  print("Perulangan ke -", i)
+
+#penggunaan for karakter
+print("Hobi Saya : ")
+listhobi = [
+  'bola', 'game mobile legend', 'badminton', 'voli', 'music'
+]
+
+for i, hobi in enumerate(listhobi):
+  print(i+1, hobi)
